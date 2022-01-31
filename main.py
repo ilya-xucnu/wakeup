8ad3540ee7830a9d371b236956de007a39bfbfcf
root <root@testubu.8.8.8.8>
2022-01-31 07:29:30 +0000

    add once call to username

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 
 import logging
 from aiogram import Bot, Dispatcher, executor, types
-from config import bot_api_token
+from config import bot_api_token, admins_list, valid_targets
 import subprocess
 logging.basicConfig(level=logging.INFO)
 
@@ -12,6 +12,21 @@ dp = Dispatcher(bot)
 async def callme(message: types.Message):
     await message.reply(message["from"].username)
     subprocess.call(["python", "call.py", message['from'].username])
+
+@dp.message_handler()
+async def once(message: types.Message):
+    text = message.text.split()
+    if text[0] == "@wakeup2bot" and text[1] == "once":
+        if message["from"].username in admins_list:
+            if text[2] in valid_targets:
+                await message.reply(f"try call to {text[2]}")
+                subprocess.call(["python", "call.py", text[2]])
+            else:
+                await message.reply("Hell knows who it is. I will not call. Ping @xucnu")
+        else:
+            await message.reply(f"I don't know you. Submit your id and username to @xucnu. Perhaps he will confirm your rights. ({message['from'].username}, {message['from'].id})")
+
+
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
